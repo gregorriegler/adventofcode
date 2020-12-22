44d70bcdefa0adac1d999a50c6fd3f593e4b3315
Gregor Riegler <gregor.riegler@ebcont.com>
2020-12-22 15:56:01 +0100

    day4 part 1 finished

--- day4/src/main/kotlin/Day4.kt
+++ day4/src/main/kotlin/Day4.kt
@@ -1,2 +1,67 @@
+import java.io.File
+
 fun main() {
+    println(day4(File("day4/input").readText()))
+}
+
+fun day4(input: String): Int {
+    return splitByEmptyLine(input)
+        .map { scanPassportData(it) }
+        .filter { valid(it) }
+        .count()
+}
+
+fun splitByEmptyLine(input: String): List<String> = input.split(Regex("\n[\\s]*\n"))
+    .map { it.trim() }
+    .filter { it.isNotEmpty() }
+
+fun scanPassportData(input: String): PassportData {
+    return input.split(Regex("[\\s]+"))
+        .map { it.trim() }
+        .filter { it.isNotEmpty() }
+        .map { parsePair(it) }
+        .let { PassportData(it) }
+}
+
+fun parsePair(input: String): Pair<Field, String> {
+    return input.split(":")
+        .map { it.trim() }
+        .let { Pair(Field.valueOf(it[0]), it[1]) }
+}
+
+fun valid(passport: PassportData): Boolean =
+    Field.values().all { it.valid(passport.value[it]) }
+
+class PassportData(input: List<Pair<Field, String>>) {
+    constructor(vararg input: Pair<Field, String>) : this(input.toList())
+
+    val value: Map<Field, String> = input.toMap()
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as PassportData
+
+        if (value != other.value) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return value.hashCode()
+    }
+
+
+}
+
+enum class Field {
+
+    byr, iyr, eyr, hgt, hcl, ecl, pid,
+
+    cid {
+        override fun valid(value: String?): Boolean = true
+    };
+
+    open fun valid(value: String?): Boolean = value != null
 }
--- day4/src/test/kotlin/Day4Test.kt
+++ day4/src/test/kotlin/Day4Test.kt
@@ -1,6 +1,5 @@
 import Field.*
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 class Day4Test {
@@ -21,7 +20,6 @@ class Day4Test {
         iyr:2011 ecl:brn hgt:59in
     """.trimIndent()
 
-    @Disabled
     @Test
     fun acceptance() {
         val result: Int = day4(passports)
@@ -85,65 +83,4 @@ class Day4Test {
     }
 }
 
-fun day4(input: String): Int {
-    return splitByEmptyLine(input)
-        .map { scanPassportData(it) }
-        .filter { valid(it) }
-        .count()
-}
 
-fun splitByEmptyLine(input: String): List<String> = input.split(Regex("\n[\\s]*\n"))
-    .map { it.trim() }
-    .filter { it.isNotEmpty() }
-
-fun scanPassportData(input: String): PassportData {
-    return input.split(Regex("[\\s]+"))
-        .map { it.trim() }
-        .filter { it.isNotEmpty() }
-        .map { parsePair(it) }
-        .let { PassportData(it) }
-}
-
-fun parsePair(input: String): Pair<Field, String> {
-    return input.split(":")
-        .map { it.trim() }
-        .let { Pair(Field.valueOf(it[0]), it[1]) }
-}
-
-fun valid(passport: PassportData): Boolean =
-    Field.values().all { it.valid(passport.value[it]) }
-
-
-class PassportData(input: List<Pair<Field, String>>) {
-    constructor(vararg input: Pair<Field, String>) : this(input.toList())
-
-    val value: Map<Field, String> = input.toMap()
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as PassportData
-
-        if (value != other.value) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return value.hashCode()
-    }
-
-
-}
-
-enum class Field {
-
-    byr, iyr, eyr, hgt, hcl, ecl, pid,
-
-    cid {
-        override fun valid(value: String?): Boolean = true
-    };
-
-    open fun valid(value: String?): Boolean = value != null
-}
