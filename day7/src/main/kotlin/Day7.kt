a38c29377233ba5643317a2761e2cfbb3436022c
Gregor Riegler <gregor.riegler@ebcont.com>
2021-01-03 22:00:19 +0100

    day7 can parse rules

--- day7/src/main/kotlin/Day7.kt
+++ day7/src/main/kotlin/Day7.kt
@@ -5,5 +5,44 @@ fun main() {
 }
 
 fun day7(input: String): Int {
-    return 0
+    val rules = input.lines()
+        .map { it.toRule() }
+
+    val result = rules.filter { it.hasContainingBag("shiny gold") }
+        .flatMap { parentsRecursively(it.bag, rules) }
+        .count()
+
+    return result
 }
+
+fun parentsRecursively(bag: String, rules: List<LuggageRule>): List<String> {
+    TODO("Not yet implemented")
+}
+
+// this should be a regex xD
+fun String.toRule() = this.split(" bags contain ").let {
+    LuggageRule(
+        it[0],
+        if (it[1].substringBefore(' ') == "no") emptyMap()
+        else
+            it[1].split(", ")
+                .map { containing ->
+                    Pair(
+                        containing.split(' ').drop(1).take(2).joinToString(" "),
+                        containing.substringBefore(' ').toInt()
+                    )
+                }.toMap()
+
+    )
+
+}
+
+data class LuggageRule(
+    val bag: String,
+    val containingBags: Map<String, Int>
+) {
+    fun hasContainingBag(bag: String): Boolean {
+        return false
+    }
+}
+
--- day7/src/test/kotlin/Day7Test.kt
+++ day7/src/test/kotlin/Day7Test.kt
@@ -1,10 +1,45 @@
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 class Day7Test {
 
+    val input = """
+                light red bags contain 1 bright white bag, 2 muted yellow bags.
+                dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+                bright white bags contain 1 shiny gold bag.
+                muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+                shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+                dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+                vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+                faded blue bags contain no other bags.
+                dotted black bags contain no other bags.    
+                """.trimIndent()
+
     @Test
+    @Disabled
     fun acceptance() {
+        assertThat(day7(this.input)).isEqualTo(4)
     }
 
+    @Test
+    fun toRule() {
+        assertThat("faded blue bags contain no other bags.".toRule())
+            .isEqualTo(LuggageRule("faded blue", emptyMap()))
+
+        assertThat("faded blue bags contain 1 shiny gold bag.".toRule())
+            .isEqualTo(LuggageRule("faded blue", mapOf(Pair("shiny gold", 1))))
+
+        assertThat("faded blue bags contain 2 shiny gold bags.".toRule())
+            .isEqualTo(LuggageRule("faded blue", mapOf(Pair("shiny gold", 2))))
+
+        assertThat("faded blue bags contain 3 dotted black bags.".toRule())
+            .isEqualTo(LuggageRule("faded blue", mapOf(Pair("dotted black", 3))))
+
+        assertThat("light red bags contain 1 bright white bag, 2 muted yellow bags.".toRule())
+            .isEqualTo(LuggageRule("light red", mapOf(
+                Pair("bright white", 1),
+                Pair("muted yellow", 2),
+            )))
+    }
 }
