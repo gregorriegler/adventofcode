b48e22b2e38b5ae0d5982b281019e9682118ca4b
Gregor Riegler <gregor.riegler@ebcont.com>
2021-01-03 19:32:24 +0100

    day7 skeleton

---
+++ day7/src/main/kotlin/Day7.kt
@@ -0,0 +1,9 @@
+import java.io.File
+
+fun main() {
+    println(day7(File("day7/input").readText()))
+}
+
+fun day7(input: String): Int {
+    return 0
+}
---
+++ day7/src/test/kotlin/Day7Test.kt
@@ -0,0 +1,10 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class Day7Test {
+
+    @Test
+    fun acceptance() {
+    }
+
+}
