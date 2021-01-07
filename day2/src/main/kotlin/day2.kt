59edd097b7a4ef9d7fd4fd0750d33fda385206b4
Gregor Riegler <gregor.riegler@ebcont.com>
2021-01-07 09:38:14 +0100

    refactor day2

--- day2/src/main/kotlin/day2.kt
+++ day2/src/main/kotlin/day2.kt
@@ -6,26 +6,22 @@ fun main() = countValidPasswords(File("day2/input.txt").readLines()) { CharPosit
 fun countValidPasswords(lines: List<String>, ruleSupplier: (String) -> PasswordRule): Int =
     lines.count { validate(it, ruleSupplier) }
 
-fun validate(line: String, ruleSupplier: (String) -> PasswordRule): Boolean {
-    val (rule, password) = split(line, ruleSupplier)
-    return rule.isValid(password)
-}
+fun validate(line: String, ruleSupplier: (String) -> PasswordRule): Boolean =
+    split(line, ruleSupplier)
+        .let { (rule, password) -> rule.isValid(password) }
 
-fun split(line: String, ruleSupplier: (String) -> PasswordRule): Pair<PasswordRule, String> {
-    val split = line.split(':')
-    return Pair(ruleSupplier.invoke(split[0]), split[1].trim())
-}
+fun split(line: String, ruleSupplier: (String) -> PasswordRule): Pair<PasswordRule, String> =
+    line.split(':').let { Pair(ruleSupplier.invoke(it[0]), it[1].trim()) }
 
 interface PasswordRule {
     fun isValid(password: String): Boolean
 }
 
 class CharsCountRule(private val spec: String) : PasswordRule {
-    override fun isValid(password: String): Boolean {
-        return password.filter { char() == it }
+    override fun isValid(password: String): Boolean =
+        password.filter { char() == it }
             .count()
             .let { it in range() }
-    }
 
     private fun range(): IntRange = spec.split(' ')[0]
         .split('-')
@@ -36,10 +32,9 @@ class CharsCountRule(private val spec: String) : PasswordRule {
 }
 
 class CharPositionRule(private val spec: String) : PasswordRule {
-    override fun isValid(password: String): Boolean {
-        return positions().filter { password.length > it && password[it] == char() }
+    override fun isValid(password: String): Boolean =
+        positions().filter { password.length > it && password[it] == char() }
             .count() == 1
-    }
 
     private fun positions() = spec.split(' ')[0]
         .split('-')
