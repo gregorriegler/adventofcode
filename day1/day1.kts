7d938f0690ade3376af5453a94e4ac94082472b5
Gregor Riegler <gregor.riegler@ebcont.com>
2021-01-07 10:47:59 +0100

    refactor day1

--- day1/day1.kts
+++ day1/day1.kts
@@ -16,7 +16,7 @@ fun main() {
     })
 
     trackDuration({
-        println("" + commonsMath("input.txt",3, 2020) + " [apache commons-math]")
+        println("" + commonsMath("input.txt", 3, 2020) + " [apache commons-math]")
     })
 
     trackDuration({
@@ -41,54 +41,47 @@ fun loop(report: String, amount: Int, orElse: Int): Int {
     }
 }
 
-fun commonsMath(report: String, n: Int, amount: Int): Int {
-    return File(report)
+fun commonsMath(report: String, n: Int, amount: Int): Int =
+    File(report)
         .readLines()
         .map { it.toInt() }
         .commonsCombinations(n)
         .first { it.hasSumEqualTo(amount) }
         .product()
-}
 
-fun recursiveGenerator(file: String, k: Int, sum: Int): Int {
-    return File(file)
+fun recursiveGenerator(file: String, k: Int, sum: Int): Int =
+    File(file)
         .readLines()
         .map { it.toInt() }
         .combinations(k)
         .first { it.hasSumEqualTo(sum) }
         .product()
-}
 
-fun <T> List<T>.commonsCombinations(k: Int): Sequence<List<T>> {
-    return combinationsIterator(this.size, k)
+fun <T> List<T>.commonsCombinations(k: Int): Sequence<List<T>> =
+    combinationsIterator(this.size, k)
         .asSequence()
         .map { it -> it.map { this[it] } }
-}
 
-fun <T> List<T>.combinations(k: Int): Sequence<List<T>> {
-    return recursiveCombinationGenerator(this, k)
-}
+fun <T> List<T>.combinations(k: Int): Sequence<List<T>> = recursiveCombinationGenerator(this, k)
 
-fun <T> recursiveCombinationGenerator(input: List<T>, k: Int, building: MutableList<T> = mutableListOf()): Sequence<List<T>> {
-    return sequence {
-        if (building.size == k) {
-            yield(building.toList())
-        } else if (input.isNotEmpty()) {
-            val first: T = input[0]
-            val rest: List<T> = input.drop(1)
-            yieldAll(recursiveCombinationGenerator(rest, k, building.plus(first).toMutableList()))
-            yieldAll(recursiveCombinationGenerator(rest, k, building))
-        }
+fun <T> recursiveCombinationGenerator(
+    input: List<T>,
+    k: Int,
+    building: MutableList<T> = mutableListOf()
+): Sequence<List<T>> = sequence {
+    if (building.size == k) {
+        yield(building.toList())
+    } else if (input.isNotEmpty()) {
+        val first: T = input[0]
+        val rest: List<T> = input.drop(1)
+        yieldAll(recursiveCombinationGenerator(rest, k, building.plus(first).toMutableList()))
+        yieldAll(recursiveCombinationGenerator(rest, k, building))
     }
 }
 
-fun List<Int>.hasSumEqualTo(expected: Int): Boolean {
-    return this.sum() == expected
-}
+fun List<Int>.hasSumEqualTo(expected: Int): Boolean = this.sum() == expected
 
-fun Iterable<Int>.product(): Int {
-    return reduce { acc, it -> acc * it }
-}
+fun Iterable<Int>.product(): Int = reduce { acc, it -> acc * it }
 
 fun trackDuration(func: () -> Unit) {
     val start = System.currentTimeMillis()
