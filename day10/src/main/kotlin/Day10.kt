14358bcafeb4630dab9878f5e13b789949e39bb9
Gregor Riegler <gregor.riegler@ebcont.com>
2021-01-24 17:57:26 +0100

    day10 solves part2

--- day10/src/main/kotlin/Day10.kt
+++ day10/src/main/kotlin/Day10.kt
@@ -2,17 +2,32 @@ import java.io.File
 
 fun main() {
     println(day10(File("day10/input").readText()))
+    println(day10part2(File("day10/input").readText()))
 }
 
 fun day10(input: String): Int {
-    return input.lines()
-        .map { it.toInt() }
-        .sorted()
-        .let { listOf(0) + it }
-        .windowed(2, 1)
-        .map { it[1] - it[0] }
+    return sortedJolts(input)
+        .zipWithNext()
+        .map { it.second - it.first }
         .groupingBy { it }
         .eachCount()
         .let { it[1]!! * (it[3]!! + 1) }
 }
 
+fun day10part2(input: String): Long {
+    val adapters = sortedJolts(input)
+    val pathsToAdapter: MutableMap<Int, Long> = mutableMapOf(0 to 1L)
+
+    adapters.drop(1).forEach {
+        pathsToAdapter[it] =
+            (1..3).map { previous -> pathsToAdapter.getOrDefault(it - previous, 0) }
+                .sum()
+    }
+
+    return pathsToAdapter.getValue(adapters.last())
+}
+
+fun sortedJolts(input: String): List<Int> = input.lines()
+    .map { it.toInt() }
+    .sorted()
+    .let { listOf(0) + it }
--- day10/src/test/kotlin/Day10Test.kt
+++ day10/src/test/kotlin/Day10Test.kt
@@ -4,12 +4,18 @@ import org.junit.jupiter.api.Test
 
 class Day10Test {
 
-    private val input = "16\n10\n15\n5\n1\n11\n7\n19\n6\n12\n4"
-
     @Test
-    fun acceptance() {
-        val result: Int = day10(input)
+    fun `acceptance part1`() {
+        val result: Int = day10("16\n10\n15\n5\n1\n11\n7\n19\n6\n12\n4")
 
         assertThat(result).isEqualTo(7 * 5)
+    }
+
+    @Test
+    fun `acceptance part2`() {
+        assertThat(day10part2("1")).isEqualTo(1)
+        assertThat(day10part2("1\n2")).isEqualTo(2)
+        assertThat(day10part2("16\n10\n15\n5\n1\n11\n7\n19\n6\n12\n4")).isEqualTo(8)
+        assertThat(day10part2("28\n33\n18\n42\n31\n14\n46\n20\n48\n47\n24\n23\n49\n45\n19\n38\n39\n11\n1\n32\n25\n35\n8\n17\n7\n9\n4\n2\n34\n10\n3")).isEqualTo(19208)
     }
 }
